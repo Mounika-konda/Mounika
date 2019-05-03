b8e166e90defd07029d67e844623afab3d63185d
Mounika-konda <kondamounika443@gmail.com>
2019-05-03 14:17:03 +0530

    new change

--- src/test/java/pagescripts/Gmailtest.java
+++ src/test/java/pagescripts/Gmailtest.java
@@ -35,20 +35,33 @@ public class Gmailtest extends Demoone
 		
 		
 		
+	}
+	
+	
+	@Test
+	public void validatelogin() throws IOException, Exception 
+	
+	{
+		propertiesReusables prconf=new propertiesReusables("C:\\Users\\Mounika\\eclipse-workspace\\migration\\config.properties");
+		
+		String url1=prconf.getPropertyValue("url");
+		
+		driver.get(url1);
+		String username=prconf.getPropertyValue("un");
 		
 		
+		Gmaillogin.enterUsername(username);
 		
+		Thread.sleep(2000);
 		
+		Gmaillogin.clicknext();
 		
-		
-		
-		
+		Thread.sleep(2000);
+
 		
 		
 		
 	}
-	
-	
 	
 	
 	
